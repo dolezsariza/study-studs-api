d75cf2f9986ee1c9c831d4d1b929a1c26d4f5864
abelHodasz <hodabi@gmail.com>
2020-02-10 11:59:19 +0100

    Basic api structure

---
+++ StudyStud/Models/StudyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyStud.Models
+{
+    public class StudyDbContext : DbContext
+    {
+        protected StudyDbContext(DbContextOptions<StudyDbContext> options):base(options){}
+    }
+}
