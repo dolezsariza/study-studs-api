fd444bdb3055bb900fbf3e07e7e5cb302fd687f1
Mate Iszlai <iszimate@gmail.com>
2020-02-11 15:29:23 +0100

    Create RegisterController

---
+++ StudyStud/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using StudyStud.Models;
+
+namespace StudyStud.Controllers
+{
+    [Route("/register")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        public RegisterController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<HttpStatusCode> Post([FromBody]Object json)
+        {
+            JObject jObject = JObject.Parse(json.ToString());
+            var user = new User { UserName = jObject.GetValue("userName").ToString(), Email = jObject.GetValue("email").ToString() };
+            var result = await _userManager.CreateAsync(user, jObject.GetValue("password").ToString());
+            if (result.Succeeded)
+            {
+                return HttpStatusCode.Created;
+            }
+            return HttpStatusCode.Conflict;
+        }
+    }
+}
