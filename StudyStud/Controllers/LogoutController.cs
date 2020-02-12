6c6a02850879156af40cc76343663473332802bb
dolezsariza <izabella.dolezsar@gmail.com>
2020-02-12 10:15:00 +0100

    added LogoutController

---
+++ StudyStud/Controllers/LogoutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudyStud.Models;
+
+namespace StudyStud.Controllers
+{
+    [Route("api/logout")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly SignInManager<User> signInManager;
+
+        public LogoutController(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
