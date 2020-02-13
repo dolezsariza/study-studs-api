a383aa5bd9a0292c07961ccbaf90ae4235a30d25
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-02-13 13:35:17 +0100

    Implement test basics

---
+++ StudyStudTests/FakeSignInManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using StudyStud.Models;
+
+
+namespace StudyStudTests
+{
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+            : base(new FakeUserManager(),
+            Substitute.For<IHttpContextAccessor>(),
+            Substitute.For<IUserClaimsPrincipalFactory<User>>(),
+            Substitute.For<IOptions<IdentityOptions>>(),
+            Substitute.For<ILogger<SignInManager<User>>>(),
+            Substitute.For<IAuthenticationSchemeProvider>(),
+            Substitute.For<IUserConfirmation<User>>())
+        { }
+    }
+}
---
+++ StudyStudTests/LoginControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using StudyStud.Models;
+using NSubstitute;
+using NUnit;
+using Microsoft.AspNetCore.Identity;
+using StudyStud.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System;
+
+namespace StudyStudTests
+{   
+    [TestFixture]
+    class LoginControllerTests
+    {
+        SignInManager<User> _signInManager = Substitute.For<FakeSignInManager>();
+        Microsoft.AspNetCore.Identity.SignInResult result = Substitute.For<Microsoft.AspNetCore.Identity.SignInResult>();
+        LoginController loginController;
+        
+
+        [Test]
+        public void TestLoginIsSuccessful()
+        {
+            loginController = new LoginController(_signInManager, Substitute.For<StudyDbContext>(new DbContextOptions<StudyDbContext>()));
+            result = Microsoft.AspNetCore.Identity.SignInResult.Success;
+            _signInManager.PasswordSignInAsync("Username", "Password69!", false, false).Returns(result);
+
+            Login login = new Login();
+            login.Username = "Username";
+            login.Password = "Password69!";
+            var expected = new OkResult();
+            var actual = loginController.Login(login).Result;
+            System.Console.WriteLine(expected);
+
+            Assert.That(expected.GetType(), Is.EqualTo(actual.GetType()));
+        }
+    }
+}
