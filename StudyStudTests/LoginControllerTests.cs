0f016930b5c1bcd7ba2e23b2d451dafeb2528f9f
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-02-13 14:04:14 +0100

    Refactor test project structure and classes

--- StudyStudTests/LoginControllerTests.cs
+++ StudyStudTests/LoginControllerTests.cs
@@ -1,39 +1,56 @@
 ﻿using NUnit.Framework;
 using StudyStud.Models;
 using NSubstitute;
-using NUnit;
 using Microsoft.AspNetCore.Identity;
 using StudyStud.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
-using System;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace StudyStudTests
 {   
     [TestFixture]
     class LoginControllerTests
     {
-        SignInManager<User> _signInManager = Substitute.For<FakeSignInManager>();
-        Microsoft.AspNetCore.Identity.SignInResult result = Substitute.For<Microsoft.AspNetCore.Identity.SignInResult>();
-        LoginController loginController;
+        SignInManager<User> _signInManager;
+        StudyDbContext _studyDbContext;
+        SignInResult signInResult;
+        LoginController _loginController;
+        Login _login;
         
+        [SetUp]
+        public void Setup()
+        {
+            _signInManager = Substitute.For<FakeSignInManager>();
+            _studyDbContext = Substitute.For<StudyDbContext>(new DbContextOptions<StudyDbContext>());
+            _loginController = new LoginController(_signInManager, _studyDbContext);
+            _login = new Login();
+            _login.Username = "Username";
+            _login.Password = "Password69!";
+        }
 
         [Test]
         public void TestLoginIsSuccessful()
         {
-            loginController = new LoginController(_signInManager, Substitute.For<StudyDbContext>(new DbContextOptions<StudyDbContext>()));
-            result = Microsoft.AspNetCore.Identity.SignInResult.Success;
-            _signInManager.PasswordSignInAsync("Username", "Password69!", false, false).Returns(result);
+            signInResult = SignInResult.Success;
+            _signInManager.PasswordSignInAsync("Username", "Password69!", false, false).Returns(signInResult);
 
-            Login login = new Login();
-            login.Username = "Username";
-            login.Password = "Password69!";
             var expected = new OkResult();
-            var actual = loginController.Login(login).Result;
-            System.Console.WriteLine(expected);
+            var actual = _loginController.Login(_login).Result;
 
-            Assert.That(expected.GetType(), Is.EqualTo(actual.GetType()));
+            Assert.AreEqual(expected.GetType(), actual.GetType());
+        }
+
+        [Test]
+        public void TestLoginIsFailed()
+        {
+            signInResult = SignInResult.Failed;
+            _signInManager.PasswordSignInAsync("Username", "Password69!", false, false).Returns(signInResult);
+
+            var expected = new UnauthorizedObjectResult(new UnauthorizedResult());
+            var actual = _loginController.Login(_login).Result;
+
+            Assert.AreEqual(expected.GetType(), actual.GetType());
         }
     }
 }
