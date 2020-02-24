4af56b52d5d97dc493bfd9a6d4dc764fc3d07c09
dolezsariza <izabella.dolezsar@gmail.com>
2020-02-24 14:31:55 +0100

    created Profile class for get requests

---
+++ StudyStud/Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyStud.Models
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string School { get; set; }
+        public string City { get; set; }
+    }
+}
