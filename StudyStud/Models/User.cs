58826b65329d5d9186554fabf351b18c33ee3798
dolezsariza <izabella.dolezsar@gmail.com>
2020-02-24 14:31:18 +0100

    added new properties to user

--- StudyStud/Models/User.cs
+++ StudyStud/Models/User.cs
@@ -8,7 +8,12 @@ namespace StudyStud.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string School { get; set; }
         public string City { get; set; }
-        public string Password { get; set; }
     }
 }
