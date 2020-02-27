591ec6c189a2bec9190169f3faed3db5d11779eb
dolezsariza <izabella.dolezsar@gmail.com>
2020-02-27 09:38:43 +0100

    changed return types of methods

--- StudyStud/Controllers/UserController.cs
+++ StudyStud/Controllers/UserController.cs
@@ -23,44 +23,48 @@ namespace StudyStud.Controllers
         [HttpGet("{userName}")]
         public async Task<IActionResult> GetProfile(string userName)
         {
-            var user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-            Profile profile = new Profile()
+            try
             {
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                NickName = user.NickName,
-                Introduction = user.Introduction,
-                Interests = user.Interests,
-                School = user.School,
-                City = user.City
-            };
-
-            return Ok(profile);
+                var user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+                Profile profile = new Profile()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    NickName = user.NickName,
+                    Introduction = user.Introduction,
+                    Interests = user.Interests,
+                    School = user.School,
+                    City = user.City
+                };
+                return Ok(profile);
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
         }
 
         [HttpPut("{userName}")]
         public async Task<IActionResult> UpdateProfile(string userName, [FromBody]User user)
         {
-            
-            var nuser = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-
-            nuser.FirstName = user.FirstName;
-            nuser.LastName = user.LastName;
-            nuser.NickName = user.NickName;
-            nuser.Introduction = user.Introduction;
-            nuser.Interests = user.Interests;
-            nuser.School = user.School;
-            nuser.City = user.City;
-
             try
             {
+                var nuser = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+
+                nuser.FirstName = user.FirstName;
+                nuser.LastName = user.LastName;
+                nuser.NickName = user.NickName;
+                nuser.Introduction = user.Introduction;
+                nuser.Interests = user.Interests;
+                nuser.School = user.School;
+                nuser.City = user.City;
                 await _context.SaveChangesAsync();
                 return Ok();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
-                return StatusCode(204);
+                return NotFound();
             }
            
         }
@@ -68,59 +72,79 @@ namespace StudyStud.Controllers
         [HttpDelete("{userName}")]
         public async Task<IActionResult> DeleteProfile(string userName)
         {
-            User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-            List<Topic> topicsOfUser = await _context.TopicList.Where(topic => topic.OwnerId == user.Id).ToListAsync();
-            List<Post> postsOfUser = await _context.PostList.Where(post => post.OwnerId == user.Id).ToListAsync();
-            
-            foreach (Topic topic in topicsOfUser)
-            {
-                topic.OwnerId = null;
-            }
-
-            foreach (Post post in postsOfUser)
-            {
-                post.OwnerId = null;
-            }
-
-            _context.UserList.Remove(user);
-            
             try
             {
+                User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+                List<Topic> topicsOfUser = await _context.TopicList.Where(topic => topic.OwnerId == user.Id).ToListAsync();
+                List<Post> postsOfUser = await _context.PostList.Where(post => post.OwnerId == user.Id).ToListAsync();
+            
+                foreach (Topic topic in topicsOfUser)
+                {
+                    topic.OwnerId = null;
+                }
+
+                foreach (Post post in postsOfUser)
+                {
+                    post.OwnerId = null;
+                }
+
+                _context.UserList.Remove(user);
                 await _context.SaveChangesAsync();
-                return Ok();
+                return NoContent();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
-                return StatusCode(204);
+                return BadRequest();
             }
         }
 
         [HttpGet("{userName}/topics")]
         public async Task<IActionResult> ListUsersTopics(string userName)
         {
-            User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-            List<Topic> topicsOfUser = await _context.TopicList.Where(topic => topic.OwnerId == user.Id).ToListAsync();
+            try
+            {
+                User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+                List<Topic> topicsOfUser = await _context.TopicList.Where(topic => topic.OwnerId == user.Id).ToListAsync();
 
-            return Ok(topicsOfUser);
+                return Ok(topicsOfUser);
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
         }
 
         [HttpGet("{userName}/posts")]
         public async Task<IActionResult> ListUsersPosts(string userName)
         {
-            User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-            List<Post> postsOfUser = await _context.PostList.Where(post => post.OwnerId == user.Id).ToListAsync();
-            
-            return Ok(postsOfUser);
+            try
+            {
+                User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+                List<Post> postsOfUser = await _context.PostList.Where(post => post.OwnerId == user.Id).ToListAsync();
+
+                return Ok(postsOfUser);
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
         }
 
         [HttpGet("{userName}/{topicId}/posts")]
         public async Task<IActionResult> ListUsersPostsInTopic(string userName,int topicId)
         {
-            User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
-            List<Post> postsOfTopic = await _context.PostList.Where(post => post.TopicID == topicId && post.OwnerId == user.Id).ToListAsync();
+            try
+            {
+                User user = await _context.UserList.SingleOrDefaultAsync(user => user.UserName == userName);
+                List<Post> postsOfTopic = await _context.PostList.Where(post => post.TopicID == topicId && post.OwnerId == user.Id).ToListAsync();
 
-            return Ok(postsOfTopic);
+                return Ok(postsOfTopic);
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
         }
     }
 }
