ac38822aaf431500129fee342aec9e2e3c29a922
Mate Iszlai <iszimate@gmail.com>
2020-03-10 15:05:17 +0100

    Add tests to GetTopic and AddTopic methods

--- StudyStudTests/TopicControllerTests.cs
+++ StudyStudTests/TopicControllerTests.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using StudyStud.Models;
-using NSubstitute;
 using Microsoft.EntityFrameworkCore;
 using StudyStud.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using StudyStudTests.Utilities;
 
 namespace StudyStudTests
 {
@@ -14,21 +14,22 @@ namespace StudyStudTests
     {
 
         List<Topic> _topics;
-        StudyDbContext _context;
+        StudyDbContext _context = new StudyDbContext(new DbContextOptionsBuilder<StudyDbContext>()
+            .UseInMemoryDatabase(databaseName: "users")
+            .Options);
         TopicController _topicController;
+        User _user;
         
 
         [SetUp]
         public void SetUp()
         {
-            var options = new DbContextOptionsBuilder<StudyDbContext>()
-            .UseInMemoryDatabase(databaseName: "users")
-            .Options;
-            var topic1 = new Topic { Description = "asdasd", Id = 0, OwnerId = "ad", OwnerName = "Sanyi", Title = "Sanyitopic" };
-            var topic2 = new Topic();
+            var topic1 = new Topic();
+            var topic2 = new Topic { Description = "asdasd", Id = 4, OwnerId = "ad", OwnerName = "Sanyi", Title = "Sanyitopic" };
             var topic3 = new Topic { Description = "aaaaaaa", Id = 3, OwnerId = "ad", OwnerName = "Sanyi", Posts = new List<Post> { new Post(), new Post { Id = 5, OwnerId = "si", OwnerName = "Kata", Message = "asda", TopicID = 3 } } };
+            _user = new User { Id = "ad", NickName = "Sanya", UserName = "Sanyi" };
+            _context.UserList.Add(_user);
             _topics = new List<Topic> { topic1, topic2, topic3 };
-            _context = new StudyDbContext(options);
             _topics.ForEach(t => _context.TopicList.Add(t));
             _context.SaveChanges();
             _topicController = new TopicController(_context);
@@ -37,15 +38,41 @@ namespace StudyStudTests
         [TearDown]
         public void TearDown()
         {
-            _context = null;
+            _context.UserList.Remove(_user);
+            _context.TopicList.RemoveRange(_topics);
+            _context.SaveChanges();
+            _topics = null;
         } 
 
         [Test]
-        public void TestGetTopicsReturnSuccess()
+        public void GetTopics_ReturnSuccess()
         {
-            var expected = new OkObjectResult(_topics);
-            var actual = _topicController.GetTopics();
-            Assert.AreEqual(expected.GetType(), actual.Result.GetType());
+            _topics.Sort(new TopicComparer());
+            _topics.Reverse();
+            var expected = new OkObjectResult(_topics).Value;
+            var actual = ((OkObjectResult)_topicController.GetTopics().Result).Value;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddTopic_GivenValidTopic_ReturnSuccess()
+        {
+            var newTopic = new Topic { OwnerId = "ad" };
+            _topics.Add(newTopic);
+            var expected = new OkResult().StatusCode;
+            var actual = ((OkResult)_topicController.AddTopic(newTopic).Result).StatusCode;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddTopic_GivenTopicWithoutOwnerId_ReturnStatusCode406()
+        {
+            var newTopic = new Topic();
+
+            var expected = new StatusCodeResult(406).StatusCode;
+            var actual = ((StatusCodeResult)_topicController.AddTopic(newTopic).Result).StatusCode;
+            
+            Assert.AreEqual(expected, actual);
         }
     }
 }
