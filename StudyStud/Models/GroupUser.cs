9c04b643bff4ff999c09de8f094873beb475dccb
dolezsariza <izabella.dolezsar@gmail.com>
2020-03-11 11:28:12 +0100

    Change UserId type from int to string in GroupUser

--- StudyStud/Models/GroupUser.cs
+++ StudyStud/Models/GroupUser.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudyStud.Models
 {
     public class GroupUser
     {
-        public int UserId { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
         public User User { get; set; }
+        [ForeignKey(nameof(Group))]
         public int GroupId { get; set; }
         public Group Group { get; set; }
     }
