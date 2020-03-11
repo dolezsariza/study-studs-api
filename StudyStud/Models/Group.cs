7b1126d5ae200bf9e0a3173e636c8f73eaf07495
Mate Iszlai <iszimate@gmail.com>
2020-03-11 11:27:12 +0100

    Create Group class

---
+++ StudyStud/Models/Group.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyStud.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<Topic> Topics { get; set; }
+        public List<User> Users { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string OwnerId { get; set; }
+        public string OwnerName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        
+        public Group()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
