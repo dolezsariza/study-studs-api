24be38293fc9a8a5aaf999d712b64bd56ebff309
dolezsariza <izabella.dolezsar@gmail.com>
2020-03-11 10:46:44 +0100

    Add GroupUser for join table

---
+++ StudyStud/Models/GroupUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyStud.Models
+{
+    public class GroupUser
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
