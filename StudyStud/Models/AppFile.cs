460ad4a5708fbcdc29d988c87acf9996abd5e605
Mate Iszlai <iszimate@gmail.com>
2020-03-24 14:24:40 +0100

    Create file model class

---
+++ StudyStud/Models/AppFile.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyStud.Models
+{
+    public class AppFile
+    {
+        public int Id { get; set; }
+
+        public byte[] Content { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string OwnerId { get; set; }
+        public int TopicId { get; set; }
+    }
+}
