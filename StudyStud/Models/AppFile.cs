74d8fe640f6eba9ff75821baf9ad33a96e9a8034
Mate Iszlai <iszimate@gmail.com>
2020-04-07 12:51:41 +0200

    Add filename property

--- StudyStud/Models/AppFile.cs
+++ StudyStud/Models/AppFile.cs
@@ -5,9 +5,8 @@ namespace StudyStud.Models
     public class AppFile
     {
         public int Id { get; set; }
-
+        public string FileName { get; set; }
         public byte[] Content { get; set; }
-
         [ForeignKey(nameof(User))]
         public string OwnerId { get; set; }
         public int TopicId { get; set; }
